c982d6794ac1cea35df68bac957f07e35bb051ca
Rajesh Muppalla <rajesh@indix.com>
2012-01-24 11:33:31 +0530

    Fixing maven issues. Adding a build plugin configuration. Adding a dummy test to TestUtils.java for maven to work (else it throws an iniatializeError

--- src/test/java/crawlercommons/test/TestUtils.java
+++ src/test/java/crawlercommons/test/TestUtils.java
@@ -18,10 +18,16 @@
 package crawlercommons.test;
 
 import crawlercommons.fetcher.http.UserAgent;
+import org.junit.Test;
 
 public class TestUtils {
     
     // User agent for when we're not doing external fetching, so we just need a fake name.
     public static final UserAgent CC_TEST_AGENT = new UserAgent("test", "test@domain.com", "http://test.domain.com");
+
+    @Test
+    public void testForMaven() {
+
+    }
 
 }
